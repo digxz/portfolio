57315b72e75d5e389d6a588296d60d1900366fb1
digxz <diegocastelliniramirez.dcr@gmail.com>
2024-05-12 21:16:23 -0300

    mobile error fixed

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,7 +9,7 @@ revelar.reveal(".titles", {
 });
 
 var typed = new Typed("#type_effect", {
-  strings: ["Student"],
+  strings: ["Student", "Programmer", "Web Developer"],
   startDelay: 1000,
   backDelay: 1500,
   typeSpeed: 150,
