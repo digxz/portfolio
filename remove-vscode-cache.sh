021a140f9f4fd4192e2c95f6c185dc37bf4bd062
digxz <diegocastelliniramirez.dcr@gmail.com>
2026-06-11 01:54:13 -0300

    estrutura de pastas

--- remove-vscode-cache.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Execute este script UMA VEZ para remover .vscode do histórico do Git
-# Depois pode deletar este arquivo.
-git rm -r --cached .vscode/
-git add .
-git commit -m "chore: remove .vscode from git tracking"
-echo "Pronto! .vscode removido do Git. A pasta local não foi apagada."
